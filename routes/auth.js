0c297a146b8739c1f94a2aa9dcc465fe5e25abb9
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-09-30 22:58:35 -0400

    Auth

--- routes/auth.js
+++ routes/auth.js
@@ -22,9 +22,7 @@ var send_response = function(res, result) {
 
 /**
  * @param req - The body must include username and password field.
- * @param res - Returns
- * {
- * }
+ * @param res - The result is the session_id
  */
 router.post("/register", function(req, res) {
   async.waterfall([
@@ -43,7 +41,7 @@ router.post("/register", function(req, res) {
     // Step 2: Check if the username already exists.
     function(username, password, callback) {
       UserAuth.find({"username": username}, function(err, results) {
-        if (err) throw err;
+        if (err) send_error(res, err); 
         if (results.length > 0) {
           send_error(res, "Username already in use!");
         } else {
@@ -54,7 +52,7 @@ router.post("/register", function(req, res) {
     // Step 3: Create a hashed password.
     function(username, password, callback) {
       bcrypt.hash(password, constants.SALT, function(err, hash_password) {
-        if (err) throw err;
+        if (err) send_error(res, err); 
         callback(null, username, hash_password);
       });
     },
@@ -66,7 +64,7 @@ router.post("/register", function(req, res) {
       });
 
       new_user.save(function(err, result) {
-        if (err) throw err;
+        if (err) send_error(res, err);
         callback(null, username);
       });
     },
@@ -74,12 +72,129 @@ router.post("/register", function(req, res) {
     function(username, callback) {
       var session = new Session({"username": username});
       session.save(function(err, result) {
-        if (err) throw err;
+        if (err) send_error(res, err);
         send_response(res, result._id.toString());
       });
     }
   ]); // End waterfall.
 }); // End register.
+
+
+/**
+ * @param req - The body must include username and password field.
+ * @param res - The result is the session_id
+ */
+router.post("/login", function(req, res) {
+  async.waterfall([
+    // Step 1: Ensure that the request contains the username and password.
+    function(callback) {
+      var username = req.body.username;
+      var password = req.body.password;
+      if (username === undefined || password === undefined) {
+        send_error(res, "POST body need username and password");
+      } else if (username.length <= 5 || password.length <= 5) {
+        send_error(res, "The username and password must be over 5 chars long");
+      } else {
+        callback(null, username, password);
+      } 
+    },
+    // Step 2: Search for the username.
+    function(username, password, callback) {
+      UserAuth.find({"username": username}, function(err, results) {
+        if (err) send_error(res, err);
+        if (results.length > 0) {
+          callback(null, username, password, results[0].hash_password);
+        } else {
+          send_error(res, "Username not found!");
+        } 
+      }); 
+    }, 
+    // Step 3: Check if the passwords match.
+    function(username, password, hash_password, callback) {
+      bcrypt.compare(password, hash_password, function(err, is_match) {
+        if (err) send_error(res, err);
+        if (is_match) {
+          callback(null, username);
+        } else {
+          send_error(res, "The password is incorrect!");
+        }
+      });
+    },
+    // Step 4: Remove old sessions.
+    function(username, callback) {
+      Session.remove({"username": username}, function(err, result) {
+        if (err) send_error(res, err);
+        callback(null, username);
+      });
+    },
+    // Step 5: Create a new session.
+    function(username, callback) {
+      var session = new Session({"username": username});
+      session.save(function(err, result) {
+        if (err) send_error(res, err);
+        send_response(res, result._id.toString());
+      });
+    }
+  ]);
+}); // End login.
+
+/**
+ * @param req - Must contain a session_id in body
+ * @param res - result is true (if session exists) or false.
+ */
+router.post("/validate_session", function(req, res) {
+  async.waterfall([
+    // Step 1: Make sure that a session_id is in the POST body.
+    function(callback) {
+      var session_id = req.body.session_id;
+      if (session_id === undefined) {
+        send_error(res, "There must be a session_id in the POST body");
+      } else {
+        callback(null, session_id);
+      }
+    },
+    // Step 2: Search for the session_id.
+    function(session_id, callback) {
+      try {
+        Session.find({"_id": new mongoose.Types.ObjectId(session_id)}, function(err, results) {
+          if (err) send_error(res, err);
+          send_response(res, (results.length > 0));
+        });
+      } catch(err) {
+        send_error(res, "Error in searching for session_id");
+      }
+    }
+  ]);
+}); // End validate_session.
+
+/**
+ * @param req - Must contain session_id in body.
+ * @param res - Result is true if the logout was successful.
+ */
+router.post("/logout", function(req, res) {
+  async.waterfall([
+    // Step 1: Ensure that session_id is in the POST body.
+    function(callback) {
+      var session_id = req.body.session_id;
+      if (session_id === undefined) {
+        send_error(res, "There must be a session_id in the POST body");
+      } else {
+        callback(null, session_id);
+      }
+    },
+    // Step 2: Delete the session_id.
+    function(session_id, callback) {
+      try {
+        Session.remove({"_id": new mongoose.Types.ObjectId(session_id)}, function(err, result) {
+          if (err) send_error(res, err);
+          send_response(res, true);
+        });
+      } catch(err) {
+        send_error(res, "Error in searching for session_id");
+      }
+    }
+  ]);
+}); // End logout.
 
 
 module.exports.initialize = function(_mongoose) {
