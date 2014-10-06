0d3addd465fcf08ada559ae46d2f9e67718a2263
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-10-06 09:45:43 -0400

    Added make tweet

--- public/js/index.js
+++ public/js/index.js
@@ -7,23 +7,8 @@
     // The button which should be clicked to logout.
     var btn_logout;
 
-    // The paragraph which displays the alert message for the "Make Tweet" button.
-    var p_tweet_alert;
-
-    // The div which displays the alert for the "Make Tweet" button.
-    var div_tweet_alert; 
-
     // The modal window for login and registering.
     var login_modal;
-
-    // The timeout for the alert messages for the "Make Tweet" button.
-    var tweet_alert_timeout;
-
-    // The text area for composing a tweet.
-    var txt_tweet;
-
-    // The button which makes the tweet. 
-    var btn_make_tweet;
 
     // The list of the tweets.
     var lst_tweets;
@@ -35,10 +20,12 @@
     var update_interval;
 
     // The object that performs authentication.
-    var authenticator = new Fritter.Authenticator();
+    var authenticator = Fritter.Authenticator();
 
     // The object that communicates with the API for making, editing, and deleting tweets.
-    var tweeter = new Fritter.Tweeter(authenticator);
+    var tweeter = Fritter.Tweeter(authenticator);
+
+    var tweet_maker;
 
     // The object that puts the tweets into lst_tweets (it is initialized in setup_handlers).
     var tweet_list;
@@ -80,9 +67,6 @@
     btn_logout = $("#btn_logout");
     p_alert = $("#p_alert");
     div_tweet_alert = $("#div_tweet_alert");
-    txt_tweet = $("#txt_tweet");
-    txt_tweet.val("");
-    btn_make_tweet = $("#btn_make_tweet");
     lst_tweets = $("#tweet_list");
     h_username = $("#h_username");
     div_tweet_alert.css("visibility", "hidden");
@@ -93,9 +77,9 @@
    * Setup handlers for UI elements.
    */
   var setup_handlers = function(callback) {
-
     // Create the TweetList object to handle putting tweets into the list.
     tweet_list = new Fritter.TweetList(tweeter, lst_tweets);
+    tweet_maker = Fritter.TweetMaker(tweeter, $("#div_make_tweet"), tweet_list);
 
     // Handle logout.
     btn_logout.click(function(e) {
@@ -108,43 +92,6 @@
       });
     });
 
-    // Handle making a tweet.
-    btn_make_tweet.click(function(e) {
-      var tweet = txt_tweet.val();
-      if (tweet.length > 140) {
-        display_tweet_alert("You need to remove " + (tweet.length - 140) + 
-          " characters before you can tweet this!");
-      } else {
-        tweet_list.make_tweet(txt_tweet.val());
-        txt_tweet.val("");
-      }
-    });
     callback(null);
   };
-
-  /**
-   * Display an alert message above the txt_tweet textarea (i.e. the textbox in which you compose
-   * tweets).
-   */
-  var display_tweet_alert = function(message) {
-    p_tweet_alert.text(message);
-    clearTimeout(tweet_alert_timeout);
-    div_tweet_alert.css("visibility", "visible");
-    tweet_alert_timeout = setTimeout(function() {
-      div_tweet_alert.css("visibility", "hidden");
-    }, 5000);
-  };
-
-  /**
-   * Display an alert in the modal window.
-   */
-  var display_modal_alert = function(message) {
-    p_alert.text(message);
-    clearTimeout(modal_alert_timeout);
-    div_alert.css("visibility", "visible");
-    modal_alert_timeout = setTimeout(function() {
-      div_alert.css("visibility", "hidden");
-    }, 5000);
-  };
-
 })(); // End closure.
---
+++ public/js/tweet_maker.js
@@ -0,0 +1,41 @@
+(function() {
+  var TweetMaker = function(tweeter, div, tweet_list) {
+    var html = new EJS({"url": "/html/tweet_maker.ejs"}).render({});
+    div.append(html);
+
+    var txt_tweet = div.find("#txt_tweet");
+    txt_tweet.val("");
+    var btn_make_tweet = div.find("#btn_make_tweet");
+    var div_tweet_alert = div.find("#div_tweet_alert");
+    div_tweet_alert.css("visibility", "hidden");
+    var p_tweet_alert = div.find("#p_tweet_alert");
+
+    var alert_timeout;
+
+    var display_alert = function(message) {
+      p_tweet_alert.text(message);
+      clearTimeout(alert_timeout);
+      div_tweet_alert.css("visibility", "visible");
+      alert_timeout = setTimeout(function() {
+        div_tweet_alert.css("visibility", "hidden");
+      }, 5000);
+    };
+
+    btn_make_tweet.click(function() {
+      var tweet = txt_tweet.val();
+      if (tweet.length > 140) {
+        display_alert("You need to remove " + (tweet.length - 140) + 
+          " characters before you can tweet this!");
+      } else {
+        tweet_list.make_tweet(txt_tweet.val());
+        txt_tweet.val("");
+      }
+    });
+
+
+    var that = {};
+    return that;
+  };
+
+  Fritter.TweetMaker = TweetMaker;
+})();
