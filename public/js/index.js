c6185bc2225361e72ed5b4c9ba877ab79b33380e
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-10-06 10:19:45 -0400

    Removed bower

--- public/js/index.js
+++ public/js/index.js
@@ -4,24 +4,14 @@
 
 (function() {
 
-    // The button which should be clicked to logout.
-    var btn_logout;
-
     // The heading which displays the username.
     var h_username;
-
-    // The interval at which the server is polled for new updates.
-    var update_interval;
 
     // The object that performs authentication.
     var authenticator = Fritter.Authenticator();
 
     // The object that communicates with the API for making, editing, and deleting tweets.
     var tweeter = Fritter.Tweeter(authenticator);
-
-    var tweet_maker;
-
-    var logout_button;
 
     // The object that puts the tweets into lst_tweets (it is initialized in setup_handlers).
     var tweet_list;
@@ -32,7 +22,7 @@
         Fritter.RouteToLogin(authenticator, callback);
       }, 
       setup_variables,
-      setup_handlers,
+      setup_views,
       set_username
     ]); // End async series.
   }); // End document ready.
@@ -45,7 +35,6 @@
     callback(null);
   };
 
-
   /**
    * Initialize the variables with their respective DOM elements.
    */
@@ -54,18 +43,17 @@
     p_alert = $("#p_alert");
     div_tweet_alert = $("#div_tweet_alert");
     h_username = $("#h_username");
-    div_tweet_alert.css("visibility", "hidden");
     callback(null);
   }; 
 
   /**
    * Setup handlers for UI elements.
    */
-  var setup_handlers = function(callback) {
+  var setup_views = function(callback) {
     // Create the TweetList object to handle putting tweets into the list.
     tweet_list = new Fritter.TweetList(tweeter, $("#tweet_list"));
-    tweet_maker = Fritter.TweetMaker(tweeter, $("#div_make_tweet"), tweet_list);
-    logout_button = Fritter.LogoutButton(authenticator, $("#div_logout_button"));
+    Fritter.TweetMaker(tweeter, $("#div_make_tweet"), tweet_list);
+    Fritter.LogoutButton(authenticator, $("#div_logout_button"));
     callback(null);
   };
 })(); // End closure.
